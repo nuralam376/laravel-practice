5c7b4bbe54ce6ccf42b0565638fe16a4013b7d0c
Nur A Alam <nuraalam376@gmail.com>
2021-01-31 09:31:14 +0600

    practiced laravel basic

---
+++ introduction/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/hello', function () {
+    // echo "Hello";
+    return ["name" => "Hello"];
+});
+
+
+Route::get("/hello/world", function () {
+    return "Hello World";
+});
+
+Route::get("/greet/{greeting}/name/{name}", function ($greeting, $name) {
+    $greeting = ucwords($greeting);
+    $name = ucwords($name);
+    // return "{$greeting} {$name}";
+    return view("info", [
+        "greet" => $greeting,
+        "name" => $name,
+        "time" => time()
+    ]);
+});
+
+Route::post("/say", function (Request $request) {
+    $name = $request->post("name");
+    return [
+        "name" => $name
+    ];
+});
