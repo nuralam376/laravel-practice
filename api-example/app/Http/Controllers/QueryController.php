2731cb1712d4a974de79f8b4cd10495d40f23a66
Nur A Alam <nuraalam376@gmail.com>
2021-02-15 09:12:38 +0600

    practiced laravel join query

--- api-example/app/Http/Controllers/QueryController.php
+++ api-example/app/Http/Controllers/QueryController.php
@@ -65,7 +65,57 @@ class QueryController extends Controller
 
         // $users = DB::table("users")->orderByRaw("updated_at - created_at desc")->get();
 
-        $users = DB::table("users")->select("name", "email")->groupByRaw("name, email")->get();
+        // $users = DB::table("users")->select("name", "email")->groupByRaw("name, email")->get();
+
+        // $users = DB::table("users")
+        //     ->join("contacts", "users.id", "=", "contacts.user_id")
+        //     ->join("orders", "users.id", "=", "orders.user_id")
+        //     ->select("users.*", "contacts.phone", "orders.price")
+        //     ->get();
+
+        // $users = DB::table("users")
+        //     ->leftJoin("posts", "users.id", "=", "posts.user_id")
+        //     ->get();
+
+        // $users = DB::table("users")
+        //     ->rightJoin("posts", "users.id", "=", "posts.user_id")
+        //     ->get();
+
+        // $users = DB::table("colors")->crossJoin("sizes")->get();
+
+        // $users = DB::table("users")->join("contacts", function ($join) {
+        //     $join
+        //         ->on("users.id", "=", "contacts.user_id")
+        //         ->orOn("users.name", "=", "contacts.name");
+        // })
+        //     ->get();
+
+        // $users = DB::table("users")
+        //     ->join("contacts", function ($join) {
+        //         $join
+        //             ->on("users.id", "=", "contacts.user__id")
+        //             ->where("contacts.user_id", ">", 5);
+        //     })
+        //     ->get();
+
+        // $posts = DB::table("posts")
+        //     ->select("user_id", DB::raw("MAX(created_at) as last_post_created"))
+        //     ->where("is_published", true)
+        //     ->groupBy("user_id");
+
+        // $users = DB::table("users")
+        //     ->joinSub($posts, "latest_post", function ($join) {
+        //         $join->on("users.id", "=", "latest_post.user_id");
+        //     })->get();
+
+        $first = DB::table("users")
+            ->whereNull("first_name")
+            ->get();
+
+        $users = DB::table("users")
+            ->whereNull("last_name")
+            ->union($first)
+            ->get();
 
         return response()->json([
             "data" => $users
